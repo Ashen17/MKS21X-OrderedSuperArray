663904a87b85956663a3c64a0fde78c1a72aa94b
Allen Shen <ashen1@stuy.edu>
2018-11-02 11:33:22 -0400

    not presentable driver fniished

---
+++ Driver.java
@@ -0,0 +1,12 @@
+public class Driver{
+  public static void main(String[] args) {
+    NoNullArrayList<String> str_ary = new NoNullArrayList<String>(10);
+    System.out.println(str_ary);
+    System.out.println(str_ary.add("HELLO"));
+    System.out.println(str_ary);
+    NoNullArrayList<Integer> int_ary = new NoNullArrayList<Integer>(6);
+    System.out.println(int_ary);
+    System.out.println(int_ary.add(5);
+    System.out.println(int_ary):
+  }
+}
