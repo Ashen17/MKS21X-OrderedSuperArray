bea2649d796332721e6723ccaac9883776c7bc8c
Allen Shen <ashen1@stuy.edu>
2018-11-02 11:46:37 -0400

    constructors done, location WIP

---
+++ OrderedArrayList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class OrderedArraylist<T extends Comparable<T>> extends NoNullArrayList<T>{
+
+  public OrderedArraylist(){}
+  public OrderedArraylist(int capacity){
+    super(capacity);
+  }
+  private int Location(T value){
+    if (size() == 0){return 0;}
+    for (int pos = size()/2; pos < size(); pos++){
+
+    }
+  }
+
+
+}
